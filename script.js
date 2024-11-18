be87b95ac815371ef9f4d36df47c8d6cfa7836db
KamogeloMahlake <3949843@myuwc.ac.za>
2024-11-18 20:54:27 +0200

    adding styles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let price = 3.26;
-let cid = [
+const cid = [
   ['PENNY', 1.01],
   ['NICKEL', 2.05],
   ['DIME', 3.1],
@@ -14,71 +14,23 @@ let cid = [
 const cash = document.getElementById("cash");
 const changeDue = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
+const availiableChangeDiv = document.getElementById("cid");
+const reversedCid = cid.reverse();
+const moneyValues = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01].map(el => el * 100)
 
-function cashRegister(price, cid) {
-  if (parseFloat(cash.value) < price) {
-    alert("Customer does not have enough money to purchase the item");
-    
-  } else if (parseFloat(cash.value) === price) {
-    changeDue.textContent = "No change due - customer paid with exact cash";
-  } else {
-    const reversedCid = cid.reverse();
-    let currentChangeAvailable = cid.reduce((arr, item) => arr + item[1], 0) * 100;
-    let change = (parseFloat(cash.value) - price) * 100;
-    const moneyValues = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01].map(el => el * 100)
-    const results = []
-    changeDue.textContent = "Status: OPEN";
-    if (change > currentChangeAvailable) {
-      changeDue.textContent = "Status: INSUFFICIENT_FUNDS";
-      return;
-  }  else {
-    for (let i =0; i < reversedCid.length; i++) {
-      if (change >= moneyValues[i] && change > 0) {
-        const [value, total] = reversedCid[i];
-        const possibleChange = Math.min(total * 100, change);
-        const count = Math.floor(possibleChange / moneyValues[i]);
-        const amountDue = count * moneyValues[i];
-        change -= amountDue;
-        currentChangeAvailable -= amountDue;
-        cid[cid.length - (1 + i)] -= amountDue;
-        if (count > 0) {
-          results.push([value, amountDue / 100]);
-        }
-      } 
-    }
-    if (currentChangeAvailable === 0) {
-    changeDue.textContent = "Status: CLOSED"
-  }
-    
-  }
-  
-  changeDue.textContent += ` ${(results.map(el => el.map(i => Number(i)? `$${i}`:i).join(": "))).join(" ")}`;
-    if (change > 0) {
-      changeDue.textContent = "Status: INSUFFICIENT_FUNDS";
-    }
-
-  }
-}
 
 purchaseBtn.addEventListener("click", ()=>{
   changeDue.textContent = "";
-  cashRegister(price, cid);
-  /*if (parseFloat(cash.value) < price) {
+  if (parseFloat(cash.value) < price) {
       alert("Customer does not have enough money to purchase the item");
       
     } else if (parseFloat(cash.value) === price) {
       changeDue.textContent = "No change due - customer paid with exact cash";
     } else {
-      const reversedCid = cid.reverse();
-      let currentChangeAvailable = cid.reduce((arr, item) => arr + item[1], 0) * 100;
+      let currentChangeAvailable = reversedCid.reduce((arr, item) => arr + item[1], 0) * 100;
       let change = (parseFloat(cash.value) - price) * 100;
-      const moneyValues = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01].map(el => el * 100)
       const results = []
       changeDue.textContent = "Status: OPEN";
-      if (change > currentChangeAvailable) {
-        changeDue.textContent = "Status: INSUFFICIENT_FUNDS";
-        return;
-    }  else {
       for (let i =0; i < reversedCid.length; i++) {
         if (change >= moneyValues[i] && change > 0) {
           const [value, total] = reversedCid[i];
@@ -89,21 +41,25 @@ purchaseBtn.addEventListener("click", ()=>{
           currentChangeAvailable -= amountDue;
           if (count > 0) {
             results.push([value, amountDue / 100]);
+            reversedCid[i][1] = Math.round((total - (amountDue / 100)) * 100) / 100
           }
         } 
       }
       if (currentChangeAvailable === 0) {
       changeDue.textContent = "Status: CLOSED"
     }
-      
-    }
     
     changeDue.textContent += ` ${(results.map(el => el.map(i => Number(i)? `$${i}`:i).join(": "))).join(" ")}`;
+    availiableChangeDiv.innerHTML = `<h3>Availiable Change</h3>${reversedCid.map(el => `<p>${el.map(i => Number(i)?`$${i}`: i).join(": ")}</p>`).join("")}`
+
       if (change > 0) {
-        changeDue.textContent = "Status: INSUFFICIENT_FUNDS";
+        changeDue.textContent = "Status: INSUFFICIENT_FUNDS" ;
+        for (let i = 0; i < reversedCid.length; i++) {
+          reversedCid[i][1] = cid[reversedCid.length - (1 + i)][1] 
+        }
       }
 
-    }*/
+    }
+    cash.value = "";
+
 })
-
-
